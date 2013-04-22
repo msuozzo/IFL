2b183fbd21fa026b76d6d3a849b366ad853344ae
Qian Yu <qy2126@columbia.edu>
2013-04-22 17:06:58 -0400

    added tester codes

--- code_example/Tester.py
+++ code_example/Tester.py
@@ -4,7 +4,12 @@ def main():
 	health = Trait()
 	health.setValue('current', 50)
 
-	print health.getValue("current")
+	health.setValue('current', 100)
+
+	player = Character()
+	player.setValue("health", health)
+
+	print "Hello, I am a character"
 
 if __name__ == '__main__':
     main()
