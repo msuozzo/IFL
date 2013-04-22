bb1629eb5969b9f1a4f185b564aeb4584d6d0b9f
John Liu <johnnnpl@gmail.com>
2013-04-22 14:04:56 -0400

    CodeGen 0.001
    Signed-off-by: John Liu <johnnnpl@gmail.com>

---
+++ code_example/Character.py
@@ -0,0 +1,11 @@
+class Character():
+
+	def __init__(self):
+		self.d = dict();
+
+	def setValue(key, value):
+		self.d[key] = value
+
+	def getValue(key):
+		return self.d[key]
+
---
+++ code_example/Trait.py
@@ -0,0 +1,11 @@
+class Trait():
+
+	def __init__(self):
+		self.d = dict();
+
+	def setValue(key, value):
+		self.d[key] = value
+
+	def getValue(key):
+		return self.d[key]
+
