2b72c2bad5cf2db1201f3b4e0eef5122e83279c0
Qian Yu <qy2126@columbia.edu>
2013-04-23 23:50:36 -0400

    modified test1.py to include tree traversal code

--- test1.py
+++ test1.py
@@ -2,6 +2,49 @@ from parser.ifl import generate_lexer
 from parser.ifl_yacc import generate_parser
 from parser.preprocessor import clean_input
 
+class Node(object):
+	def __init__(self, data):
+		self.data = data
+		self.children = []
+
+	def add_child(self, object):
+		self.children.append(object)
+
+	def add_child_front(self, object):
+		self.children.insert(0, object)
+
+	def get_children(self):
+		return self.children
+
+def debug(node, tabs_count = 0):
+	"""Prints out all of the children of a Node."""
+	text = ""
+	tabs = " "
+	for i in range(tabs_count):
+		tabs = tabs + "\t"
+
+	for n in node.get_children():
+		if type(n) is Node:
+			text = text + tabs + debug(n, tabs_count + 1)
+			text = text + "\n"
+		else:
+			text = text + "\n" + tabs + n
+
+	return text
+
+def construct_tree(data):
+	"""Create a tree based on the input from the parser."""
+	current_parent = Node("parent")
+
+	for token in data:
+		if type(token) is tuple:
+			n = construct_tree(token)
+			current_parent.add_child(n)
+		else:
+			current_parent.add_child(token)
+
+	return current_parent
+
 lexer, tokens = generate_lexer()
 
 parser = generate_parser(lexer, tokens)
@@ -15,5 +58,31 @@ while True:
     if not tok: break
     print tok
 
-print parser.parse(cleaned_data)
+tree = parser.parse(cleaned_data)
+
+t = construct_tree(tree)
+
+for a in t.get_children():
+	for b in a.get_children():
+		print b
+	print "\n"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
