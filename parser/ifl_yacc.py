9bb08b4642da84819f8e60891fe0de33e2800d5b
Matthew Suozzo <matthew.suozzo@gmail.com>
2013-05-09 17:28:15 -0400

    Fixed introduced grammar bug, fixed name checking errors

--- parser/ifl_yacc.py
+++ parser/ifl_yacc.py
@@ -186,8 +186,8 @@ def generate_parser(lexer, tokens):
   def p_add(p):
     '''add : ADD quantity ID TO object_chain
            | ADD primitive TO object_chain'''
-    if len(p) == 5: p[0] = (p[1], p[2], p[3], p[4])
-    else: p[0] = (p[1], None, p[2], p[3])
+    if len(p) == 6: p[0] = (p[1], p[2], p[3], p[5])
+    else: p[0] = (p[1], None, p[2], p[4])
 
   def p_quantity(p):
     '''quantity : LBRACK arithmetic_expression RBRACK
