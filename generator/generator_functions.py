3e0671c911e05f8795a0202a6492b81cecb58229
John Liu <johnnnpl@gmail.com>
2013-05-10 11:07:27 -0400

    .
    Signed-off-by: John Liu <johnnnpl@gmail.com>

--- generator/generator_functions.py
+++ generator/generator_functions.py
@@ -60,11 +60,50 @@ def generate_add(n):
 
 
 
-# def generate_set(n):
-#     return "self." + n(target) + "=" + n(value) +"\n"
-#
-# def generate_decrease(n):
-#     return "self." + n(target) + "+="  + n(value) +"\n"
-#
-# def generate_increase(n):
-#     return "self." + n(target) + "-="  + n(value) +"\n"
+def generate_increase(n):
+	targ = ""
+
+	if(len(n["target"]) > 1):
+    	temp = 0
+    	for iString in n["target"]:
+        	if(iString == "SELF"):
+            	iString = iString.lower()
+
+        	if(temp == 0):
+            	targ = targ + iString
+            	temp += 1
+        	else:
+            	targ = targ + "." + iString
+
+    else: # only one target
+        if n["target"][0] == "SELF":
+            targ = n["target"][0].lower()
+        else:
+            targ = n["target"][0]
+
+	return "" + targ + "+=" + n["value"] +"\n"
+
+
+
+def generate_decrease(n):
+	targ = ""
+
+	if(len(n["target"]) > 1):
+    	temp = 0
+    	for iString in n["target"]:
+        	if(iString == "SELF"):
+            	iString = iString.lower()
+
+        	if(temp == 0):
+            	targ = targ + iString
+            	temp += 1
+        	else:
+            	targ = targ + "." + iString
+
+    else: # only one target
+        if n["target"][0] == "SELF":
+            targ = n["target"][0].lower()
+        else:
+            targ = n["target"][0]
+
+	return "" + targ + "-=" + n["value"] +"\n"
