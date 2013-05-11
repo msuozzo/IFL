bdad7b212d230988fbfb569d5204116ce9ccb42c
Michael <michaelyan2@gmail.com>
2013-05-11 14:04:14 -0400

    Reverted generator_functions to correct version

--- generator/generator_functions.py
+++ generator/generator_functions.py
@@ -50,10 +50,11 @@ class FunctionGenerator():
         elif self.get_type(node.id_) == 'ITEM':
             original_count = "{target}.items[{id_}][1]".format(target=target, id_=node.id_)
             new_thing = node.id_.capitalize() + "()"
-            return_stmt ="if {id_} in {target}.items:\n" \
-                        "\t{target}.items['{id_}'][1] = {original_count} + {quantity}\n" \
-                   "else:\n" \
-                        "\t{target}.items['{id_}'] = ({new_item}, {quantity})\n".format(target=target, id_=node.id_, original_count = original_count, quantity=node.quant, new_item=new_thing)
+            return_stmt =\
+                "if {id_} in {target}.items:\n" \
+                    "\t{target}.items['{id_}'][1] = {original_count} + {quantity}\n" \
+                "else:\n" \
+                    "\t{target}.items['{id_}'] = ({new_item}, {quantity})\n".format(target=target, id_=node.id_, original_count = original_count, quantity=node.quant, new_item=new_thing)
         else:
             attr = node.id_
             new_thing = node.id_.capitalize() + "()"
@@ -69,46 +70,20 @@ class FunctionGenerator():
         return return_stmt
 
     def generate_remove(self, node):
-        target = self.resolve_target(node.target)
-
-        return_stmt = ""
-
-        if self.get_type(node.id_) == 'ITEM':
-            original_count = "{target}.items[{id_}][1]".format(target=target, id_=node.id_)
-            new_count = original_count - 1
-            return_stmt ="if {id_} in {target}.items:\n" \
-                        "\t{target}.items['{id_}'][1] = {new_count}\n".format(target=target, id_=node.id_, new_count = new_count)
-        elif node.primitive:
-            attr = node.primitive.name
-            return_stmt = "del {target}.{attr}".format(target=target, attr=attr)
-
-        return return_stmt
+        return "pass\n"
 
     # moves player
     def generate_move(self, node):
-        return "generate move"
+        return "pass\n"
 
     def generate_execute(self, node):
-        return "generate execute"
+        return "pass\n"
 
-    def generate_function(self, node):
-        pass
+    def generate_increase(self, node):
+        return "pass\n"
 
-    def generate_statement(self, node, level):
-        stmt_map = {
-            'ADD': self.generate_add,
-            'PRINT': self.generate_print,
-            'SET': self.generate_set,
-            'REMOVE': self.generate_remove,
-            'EXECUTE': self.generate_execute,
-            'MOVE': self.generate_move
-        }
-
-        tabs = level * '\t'
-        if node.__class__.__name__ == 'Conditional':
-            return tabs + self.generate_conditionals(node, level)
-        else:
-            return tabs + stmt_map[node.type_](node)
+    def generate_decrease(self, node):
+        return "pass\n"
 
     def parse_tf_expr(self, expr):
         if expr[0] == 'HAS':
@@ -117,10 +92,26 @@ class FunctionGenerator():
             #TODO parse other TF expressions here
             pass
 
-    def generate_conditionals(self, case_inst, level):
+    def generate_statement(self, node):
+        stmt_map = {
+            'ADD': self.generate_add,
+            'PRINT': self.generate_print,
+            'SET': self.generate_set,
+            'REMOVE': self.generate_remove,
+            'EXECUTE': self.generate_execute,
+            'MOVE': self.generate_move,
+            'INCREASE': self.generate_increase,
+            'DECREASE': self.generate_decrease,
+        }
+
+        if node.__class__.__name__ == 'Conditional':
+            return self.generate_conditionals(node)
+        else:
+            return stmt_map[node.type_](node)
+
+    def generate_conditionals(self, case_inst):
         cases = case_inst.cases
         output = ""
-        tabs = level * '\t'
 
         for (counter, conditional) in enumerate(cases):
             if counter == 0:
@@ -129,12 +120,16 @@ class FunctionGenerator():
                 output += "if {condition}:\n".format(condition=self.parse_tf_expr(if_condition))
 
                 for stmt in if_stmt_list:
-                    output += self.generate_statement(stmt, level + 1)
+                    s = self.generate_statement(stmt)
+                    for line in s.splitlines():
+                        output += "\t" + line + "\n"
             elif counter == len(cases) - 1 and True:
                 else_stmt_list = cases[-1][1]
                 output += 'else:\n'
                 for stmt in else_stmt_list:
-                    output += self.generate_statement(stmt, level + 1)
+                    s = self.generate_statement(stmt)
+                    for line in s.splitlines():
+                        output += "\t" + line + "\n"
             else:
                 elif_condition = cases[counter][0]
                 elif_stmt_list = cases[counter][1]
@@ -145,93 +140,20 @@ class FunctionGenerator():
     def generate_action(self, action_phrase, stmt_list):
         output = "def {action_phrase}():".format(action_phrase=action_phrase)
         for stmt in stmt_list:
-            output += "\t" + self.generate_statement(stmt, 0)
+            s = self.generate_statement(stmt)
+            for line in s.splitlines():
+                output += "\t" + line + "\n"
         return output
 
-    # def generate_function(name, arg_names, statement_list, id, tree):
-    #
-    #     function_string = "def %s(self" % name
-    #     for arg in arg_names:
-    #         function_string += ", " + arg
-    #     function_string += "):\n"
-    #
-    #     for statement in statement_list:
-    #         s = generate_code(statement, id, tree)
-    #         for line in s.splitlines():
-    #             function_string += "\t" + line + "\n"
-    #
-    #
-    #     function_string += "\tpass\n"
-    #     return function_string
+    def generate_function(self, node):
+        function_string = "def %s(self" % node.name
+        for arg in node.arg_names:
+            function_string += ", " + arg
+        function_string += "):\n"
 
+        for stmt in node.statements:
+            s =  self.generate_statement(stmt)
+            for line in s.splitlines():
+                function_string +=  '\t' + line + '\n'
 
-# above works for sure
-
-# def generate_append(n):
-#
-#     targ=""
-# 	if(len(n.target) > 1):
-# 		temp = 0
-# 		for iString in n.target:
-# 			if(iString == "SELF"):
-# 				iString = iString.lower()
-#
-# 			if (temp == 0):
-# 				targ = targ + iString
-# 				temp += 1
-# 			else:
-# 				targ = targ + "." + iString
-#
-# 		else:
-# 			if (n.target[0] == "SELF"):
-# 				targ = n.target[0].lower()
-# 			targ = n.target[0]
-#
-# 	return "" + targ + ".appened(" + n["X"] + ")" +"\n"
-#
-#
-# def generate_increase(n):
-#     targ = ""
-#
-#     if(len(n.target) > 1):
-#         temp = 0
-#         for iString in n.target:
-#             if(iString == "SELF"):
-#                 iString = iString.lower()
-#
-#             if(temp == 0):
-#                 targ = targ + iString
-#                 temp += 1
-#             else:
-#                 targ = targ + "." + iString
-#
-#     else:
-#         if (n.target[0] == "SELF"):
-#             targ = n.target[0].lower()
-#         targ = n.target[0]
-#
-#     return "" + targ + "+=" + n.val[1] +"\n"
-#
-#
-#
-# def generate_decrease(n):
-#     targ = ""
-#
-#     if(len(n["target"]) > 1):
-#         temp = 0
-#         for iString in n.target:
-#             if(iString == "SELF"):
-#                 iString = iString.lower()
-#
-#             if(temp == 0):
-#                 targ = targ + iString
-#                 temp += 1
-#             else:
-#                 targ = targ + "." + iString
-#
-#     else:
-#         if (n.target[0] == "SELF"):
-#             targ = n.target[0].lower()
-#         targ = n.target[0]
-#
-#     return "" + targ + "-=" + n.val[1] +"\n"
+        return function_string
