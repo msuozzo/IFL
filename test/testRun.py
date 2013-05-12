5498144f68eeb7eb312dc0798da3ed2c99815596
KayFabe <hlf2119@columbia.edu>
2013-05-12 15:13:37 -0300

    Update testRun.py

--- test/testRun.py
+++ test/testRun.py
@@ -2,7 +2,7 @@ import unittest
 import os
 
 def suite(): 
-  suite = unittest.TestSuite()
+  	suite = unittest.TestSuite()
 	suite.addTest(TestCase('test-1.py'))
 	suite.addTest(TestCase('test-2.py'))
 	suite.addTest(TestCase('test-3.py'))
@@ -13,19 +13,25 @@ def suite():
 
 class SuccessTest(unittest.TestCase): 
 	def test_succ1(self): 
-		os.system("python test-1.py")
+		os.system("python compiler.py examples/ex1.ifl")
 	def test_succ2(self):
-		os.system("python test-2.py")
+		os.system("python compiler.py examples/ex2.ifl")
 	def test_succ3(self): 
-		os.system("pyton test-3.py")
+		os.system("pyton compiler.py examples/ex3.ifl")
 
 class ExpectedFailureTestCase(unittest.TestCase): 
 	def test_fail(self): 
-		os.system("python test-4.py")
+		os.system("python compiler.py test/test4.ifl")
 	def test_fail2(self): 
-		os.system("python test-5.py")
-	def test_fail3(self): 
-		os.system("python test-6.py")
+		os.system("python compiler.py test/test5.ifl")
+	def test_fail3(self):
+		os.system("python compiler.py test/test6.ifl")
+	def test_fail4(self): 
+		os.system("python compiler.py test/test7.ifl")
+	def test_file5(self): 
+		os.system("python compiler.py test/test8.ifl")
+	def test_file6(self): 
+		os.system("python compiler.py test/test9.ifl")
 		
 
 if __name__ == '__main__': 
