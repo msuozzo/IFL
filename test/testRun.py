5c1f2861b126bdfebe5dac40bd43deeb7019e6d1
KayFabe <hlf2119@columbia.edu>
2013-05-11 20:25:43 -0400

    Update testRun.py

--- test/testRun.py
+++ test/testRun.py
@@ -3,29 +3,29 @@ import os
 
 def suite(): 
   suite = unittest.TestSuite()
-	suite.addTest(TestCase('test1.py'))
-	suite.addTest(TestCase('test2.py'))
-	suite.addTest(TestCase('test3.py'))
-	suite.addTest(TestCase('test4.py'))	
-	suite.addTest(TestCase('test5.py'))
-	suite.addTest(TestCase('test6.py'))
+	suite.addTest(TestCase('test-1.py'))
+	suite.addTest(TestCase('test-2.py'))
+	suite.addTest(TestCase('test-3.py'))
+	suite.addTest(TestCase('test-4.py'))	
+	suite.addTest(TestCase('test-5.py'))
+	suite.addTest(TestCase('test-6.py'))
 	return suite
 
 class SuccessTest(unittest.TestCase): 
 	def test_succ1(self): 
-		os.system("python test1.py")
+		os.system("python test-1.py")
 	def test_succ2(self):
-		os.system("python test2.py")
+		os.system("python test-2.py")
 	def test_succ3(self): 
-		os.system("pyton test3.py")
+		os.system("pyton test-3.py")
 
 class ExpectedFailureTestCase(unittest.TestCase): 
 	def test_fail(self): 
-		os.system("python test4.py")
+		os.system("python test-4.py")
 	def test_fail2(self): 
-		os.system("python test5.py")
+		os.system("python test-5.py")
 	def test_fail3(self): 
-		os.system("python test6.py")
+		os.system("python test-6.py")
 		
 
 if __name__ == '__main__': 
