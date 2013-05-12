12d15762ecd6d0d1c59fe422f3c24f2060cbe45e
Michael <michaelyan2@gmail.com>
2013-05-12 17:55:09 -0400

    removed test files

--- test/test1.py
+++
@@ -1,8 +0,0 @@
-from parser.ifl import generate_lexer
-lexer = generate_lexer()[0]
-data = open("examples/ex1.ifl").read()
-lexer.input(data)
-while True:
-  tok = lexer.token()
-  if not tok: break
-  print tok
---
+++ testRun.py
@@ -0,0 +1,42 @@
+import unittest
+import os
+
+def suite(): 
+  	suite = unittest.TestSuite()
+	suite.addTest(TestCase('test1.ifl'))
+	suite.addTest(TestCase('test2.ifl'))
+	suite.addTest(TestCase('test3.ifl'))
+	suite.addTest(TestCase('test4.ifl'))	
+	suite.addTest(TestCase('test5.ifl'))
+	suite.addTest(TestCase('test6.ifl'))
+	suite.addTest(TestCase('test7.ifl')) 
+	suite.addTest(TestCase('test8.ifl')) 
+	suite.addTest(TestCase('test9.ifl'))
+	return suite
+
+class SuccessTest(unittest.TestCase): 
+	def test_succ1(self): 
+		os.system("python compiler.py examples/ex1.ifl")
+	def test_succ2(self):
+		os.system("python compiler.py examples/ex2.ifl")
+	def test_succ3(self): 
+		os.system("pyton compiler.py examples/ex3.ifl")
+
+class ExpectedFailureTestCase(unittest.TestCase): 
+	def test_fail(self): 
+		os.system("python compiler.py test/test4.ifl")
+	def test_fail2(self): 
+		os.system("python compiler.py test/test5.ifl")
+	def test_fail3(self):
+		os.system("python compiler.py test/test6.ifl")
+	def test_fail4(self): 
+		os.system("python compiler.py test/test7.ifl")
+	def test_file5(self): 
+		os.system("python compiler.py test/test8.ifl")
+	def test_file6(self): 
+		os.system("python compiler.py test/test9.ifl")
+		
+
+if __name__ == '__main__': 
+	unittest.main()
+ 
